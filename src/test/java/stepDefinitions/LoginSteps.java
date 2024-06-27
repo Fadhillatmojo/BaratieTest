017cc11fdac86db0cc5b70084322e2d93d0a8959
User <fadhillatmojo@gmail.com>
2024-06-27 12:34:20 +0700

    Update: login logout steps and manual testing added

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -18,26 +18,46 @@ public class LoginSteps {
     @Given("admin on the login page")
     public void admin_on_the_login_page() {
         // setup
-        driver.get("http://127.0.0.1:8000/admin/index");
+        try {
+            driver.get("http://127.0.0.1:8000/admin/index");
+            Hooks.loginTest.pass("Success directed to login page");
+        } catch (Exception e) {
+            Hooks.loginTest.fail("Fail directed to login page");
+        }
     }
 
     @When("admin sign in with correct credentials")
     public void admin_sign_in_with_correct_credentials() {
-        LoginPage login = new LoginPage(driver);
-        login.enterUsername("admin");
-        login.enterPassword("rahasia123");
+        try {
+            LoginPage login = new LoginPage(driver);
+            login.enterUsername("admin");
+            login.enterPassword("rahasia123");
+            Hooks.loginTest.pass("Success enter credentials");
+        } catch (Exception e) {
+            Hooks.loginTest.fail("Fail enter credentials");
+        }
     }
     @And("admin clicks login button")
     public void adminClicksLoginButton() {
-        LoginPage login = new LoginPage(driver);
-        login.clickLogin();
+        try {
+            LoginPage login = new LoginPage(driver);
+            login.clickLogin();
+            Hooks.loginTest.pass("Success click login button");
+        } catch (Exception e) {
+            Hooks.loginTest.fail("Fail click login button");
+        }
     }
 
     @Then("admin should redirected to the dashboard Company page")
     public void admin_should_redirected_to_the_dashboard_company_page() {
-        CompanyPage companyPage = new CompanyPage(driver);
-        String expectedUrl = "http://127.0.0.1:8000/admin/company";
-        Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+        try {
+            CompanyPage companyPage = new CompanyPage(driver);
+            String expectedUrl = "http://127.0.0.1:8000/admin/company";
+            Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+            Hooks.loginTest.pass("Success directed to dashboard admin");
+        } catch (Exception e) {
+            Hooks.loginTest.fail("Fail directed to dashboard admin");
+        }
     }
 
     @After()
--- src/test/java/stepDefinitions/LogoutSteps.java
+++ src/test/java/stepDefinitions/LogoutSteps.java
@@ -18,49 +18,84 @@ public class LogoutSteps {
 
     @Given("admin is on the login page")
     public void admin_is_on_the_login_page() {
-        driver.get("http://127.0.0.1:8000/admin/index");
+        try {
+            driver.get("http://127.0.0.1:8000/admin/index");
+            Hooks.logoutTest.pass("Success directed to login page");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail directed to login page");
+        }
     }
 
     @When("admin log in with correct credentials")
     public void admin_log_in_with_correct_credentials()
     {
-        LoginPage login = new LoginPage(driver);
-        login.enterUsername("admin");
-        login.enterPassword("rahasia123");
-        login.clickLogin();
+        try {
+            LoginPage login = new LoginPage(driver);
+            login.enterUsername("admin");
+            login.enterPassword("rahasia123");
+            login.clickLogin();
+            Hooks.logoutTest.pass("Success to enter credentials");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Success to enter credentials");
+        }
     }
 
     @Then("admin must be directed to the company dashboard")
     public void admin_must_be_directed_to_the_company_dashboard() {
-        CompanyPage companyPage = new CompanyPage(driver);
-        String expectedUrl = "http://127.0.0.1:8000/admin/company";
-        Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+        try {
+            CompanyPage companyPage = new CompanyPage(driver);
+            String expectedUrl = "http://127.0.0.1:8000/admin/company";
+            Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+            Hooks.logoutTest.pass("Success directed to the company dashboard");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail directed to the company dashboard");
+        }
     }
 
 
     @Given("admin logged in as admin")
     public void admin_logged_in_as_admin(){
-        CompanyPage companyPage = new CompanyPage(driver);
-        String expectedUrl = "http://127.0.0.1:8000/admin/company";
-        Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+        try {
+            CompanyPage companyPage = new CompanyPage(driver);
+            String expectedUrl = "http://127.0.0.1:8000/admin/company";
+            Assert.assertEquals(expectedUrl, companyPage.getActualUrl());
+            Hooks.logoutTest.pass("Success logged in as admin");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail logged in as admin");
+        }
     }
 
     @When("admin clicks the logout button")
     public void admin_click_the_logout_button(){
-        CompanyPage companyPage = new CompanyPage(driver);
-        companyPage.clickLogout();
+        try {
+            CompanyPage companyPage = new CompanyPage(driver);
+            companyPage.clickLogout();
+            Hooks.logoutTest.pass("Success click logout button");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail click logout button");
+        }
     }
     @And("admin clicks sure logout")
     public void adminClicksSureLogout() {
-        CompanyPage companyPage = new CompanyPage(driver);
-        companyPage.clickSureLogout();
+        try {
+            CompanyPage companyPage = new CompanyPage(driver);
+            companyPage.clickSureLogout();
+            Hooks.logoutTest.pass("Success click sure logout button");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail click sure logout button");
+        }
     }
     @Then("admin must be directed to the login page")
     public void admin_must_be_directed_to_the_login_page(){
-        LoginPage loginPage = new LoginPage(driver);
-        System.out.println(loginPage.getActualUrl());
-        String expectedUrl = "http://127.0.0.1:8000/admin/index";
-        Assert.assertEquals(expectedUrl, loginPage.getActualUrl());
+        try {
+            LoginPage loginPage = new LoginPage(driver);
+            System.out.println(loginPage.getActualUrl());
+            String expectedUrl = "http://127.0.0.1:8000/admin/index";
+            Assert.assertEquals(expectedUrl, loginPage.getActualUrl());
+            Hooks.logoutTest.pass("Success logout and Success directed to the login page");
+        } catch (Exception e) {
+            Hooks.logoutTest.fail("Fail logout and Fail directed to the login page");
+        }
     }
 
 
